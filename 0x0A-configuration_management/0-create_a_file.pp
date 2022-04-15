53a31bb85a49b6ec42785d5724862f420edf0349
stellarAlien <saifgaida@gmail.com>
2022-04-15 22:20:52 +0100

    0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
 # a file in tmp with ower www-data
 file{'/tmp/school':
-    mode  => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    source  => '/tmp/school'
+    content => 'I love Puppet'
 }
