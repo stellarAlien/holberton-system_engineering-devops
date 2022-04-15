f32b375d9b91798e1363b3d6e8b30299d367819f
stellarAlien <saifgaida@gmail.com>
2022-04-15 22:26:28 +0100

    ensure

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # a file in tmp with ower www-data
-file{'/tmp/school':
+file { '/tmp/school':
+    ensure  => 'file',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    source  => '/tmp/school'
+    #source  => '/tmp/school',
     content => 'I love Puppet'
 }
