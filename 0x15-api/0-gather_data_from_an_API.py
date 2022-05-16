a4376ba8fda924975e385938571a27a640c0aa09
stellarAlien <saifgaida@gmail.com>
2022-05-16 23:51:00 +0100

    simpler

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,45 +1,31 @@
 #!/usr/bin/python3
-    """_summary_
-        return each employee's todo list
+"""_summary_
+    return each employee's todo list
     Returns:
-        
-    """
+        employee progress    
+"""
 import requests
 import sys
 
-base_url = 'https://jsonplaceholder.typicode.com/'
-
-
-def do_request():
-    '''Performs request'''
-    if len(sys.argv) < 2:
-        return print('USAGE:', __file__, '<employee id>')
-    eid = sys.argv[1]
-    try:
-        id = int(sys.argv[1])
-    except ValueError:
-        return print('Employee id must be an integer')
-
-    response = requests.get(base_url + 'users/' + id)
-    if response.status_code == 404:
-        return print('User id not found')
-    elif response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    user = response.json()
-
-    response = requests.get(base_url + 'todos/')
-    if response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    todos = response.json()
-
-    user_todos = [todo for todo in todos
-                  if todo.get('userId') == user.get('id')]
-    completed = [todo for todo in user_todos if todo.get('completed')]
-    print('Employee', user.get('name'),
-          'is done with tasks({}/{}):'.
-          format(len(completed), len(user_todos)))
-    [print('\t', todo.get('title')) for todo in completed]
-
 
 if __name__ == '__main__':
-    do_request()
+
+    id = sys.argv[1]
+    title = []
+    com = 0
+    total = 0
+    url = "https://jsonplaceholder.typicode.com/users/" + id
+    result = requests.get(url).json()
+    name = result.get('name')
+    url = "https://jsonplaceholder.typicode.com/todos/"
+    result2 = requests.get(url).json()
+    for r in result2:
+        if r.get('userId') == int(id):
+            if r.get('completed') is True:
+                title.append(r['title'])
+                com += 1
+            total += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, com, total))
+    for s in title:
+        print("\t {}".format(s))
