1eba67a101b4cdc5a07df0e74fc68b22749cca89
stellarAlien <saifgaida@gmail.com>
2022-06-01 00:01:30 +0100

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""_summary_ 
+"""_summary_
         ask for subscribers of a certain subreddit
 """
-#import praw
 import requests
+
 
 def number_of_subscribers(subreddit):
     """_summary_
-    try to fetch information about subreddit 
+    try to fetch information about subreddit
     tried to do it with praw but extra credentials are required
 
     Args:
@@ -16,17 +16,22 @@ def number_of_subscribers(subreddit):
     Returns:
         _type_: 0 or nubmer of subscribers
     """
-    #user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
-    #s = praw.Reddit(user_agent)
+    '''
+     #praw style
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+    AppleWebKit/537.36 (KHTML, like Gecko)
+    Chrome/101.0.4951.67 Safari/537.36"
+    s = praw.Reddit(user_agent)
     #r = requests.get('reddit.com/r/' + subreddit)
     #if ((r.status_code % 100) == 3 or r.status_code == 404):
         #return 0
     #return s.get_subreddit(subreddit).subscribers
+    '''
     if (type(subreddit) is not str):
-            return(0)
+        return(0)
     url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
     headers = {'user-agent': 'safari:holberton/0.1.0'}
     response = requests.get(url_api, headers=headers)
     if response.status_code != 200:
         return(0)
-    return(response.json().get("data").get("subscribers"))                            
+    return(response.json().get("data").get("subscribers"))
