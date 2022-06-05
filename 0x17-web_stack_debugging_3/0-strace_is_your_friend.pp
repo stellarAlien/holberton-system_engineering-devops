ce9246645b9e295568838f8319142d95e5254311
stellarAlien <saifgaida@gmail.com>
2022-06-05 22:20:20 +0100

    provider

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix php file name with regex
-exec {'fix wordpress':
-  command  => 'sed -i s/phpp/php/g var/www/html/wp-settings.php',
+exec { 'flask':
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   provider => 'shell',
 }
