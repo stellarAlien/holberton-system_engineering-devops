e5fd061272eb4d827195d411a36d4f6af3eafcea
root <root@DESKTOP-UKA4A0D.localdomain>
2022-08-12 22:10:28 +0100

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet-lint
-package {'puppet-lint':
-    ensure   => '2.5.0',
-    provider => 'gpsem',
-}
+# install flask
+  exec { 'install python packages':
+    command => 'pip3 install flask==2.1.0 flask_restful',
+    path    => ['/usr/bin/'],
+    unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
+  }
